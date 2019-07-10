f2c2d813d8164a439e3be867328a1d001d38708d
Indunil1992 <indunil@adroitlogic.com>
2019-07-10 10:33:34 +0530

    ko

--- indunilvirginia/lambda.js
+++ indunilvirginia/lambda.js
@@ -3,9 +3,8 @@ let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
-
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/testdemo`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/demoindu.fifo`,
         AttributeNames: ['All'],
         MaxNumberOfMessages: '10',
         VisibilityTimeout: '30',
@@ -15,25 +14,24 @@ exports.handler = function (event, context, callback) {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
                 let receivedMessages = receivedMsgData.Messages;
                 receivedMessages.forEach(message => {
-                     console.log("Success with message");
-                 console.log( message );
+                     console.log("Success with message ");
+                 console.log( data );
                     // your logic to access each message through out the loop. Each message is available under variable message 
                     // within this block
                 });
             } else {
-                 console.log("Success");
+                 console.log("Success with no msg");
                  //console.log( data );
                 // No messages to process
             }
         })
         .catch(err => {
-             console.log("Success with err");
+             console.log("not Success");
                  console.log( err );
             // error handling goes here
         });
 
 
 
-
-    callback(null, { "message": "Successfully executed get " });
+    callback(null, { "message": "Successfully executed with nw fifo " });
 }
