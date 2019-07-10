310bb9c32cc41f6927ee0ee6eeb7683347f2ba73
Indunil1992 <indunil@adroitlogic.com>
2019-07-10 10:13:16 +0530

    ds

--- indunilvirginia/lambda.js
+++ indunilvirginia/lambda.js
@@ -5,9 +5,9 @@ const sqs = new SL_AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
 
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/testdemo`,
         AttributeNames: ['All'],
-        MaxNumberOfMessages: '1',
+        MaxNumberOfMessages: '10',
         VisibilityTimeout: '30',
         WaitTimeSeconds: '0'
     }).promise()
@@ -15,22 +15,24 @@ exports.handler = function (event, context, callback) {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
                 let receivedMessages = receivedMsgData.Messages;
                 receivedMessages.forEach(message => {
-                    console.log("Success");
+                     console.log("Success with message");
                  console.log( message );
                     // your logic to access each message through out the loop. Each message is available under variable message 
                     // within this block
                 });
             } else {
-                console.log("No messages to Success");
+                 console.log("Success");
                  //console.log( data );
                 // No messages to process
             }
         })
         .catch(err => {
-            console.log("not Success");
+             console.log("Success with err");
                  console.log( err );
             // error handling goes here
         });
+
+
 
 
     callback(null, { "message": "Successfully executed get " });
