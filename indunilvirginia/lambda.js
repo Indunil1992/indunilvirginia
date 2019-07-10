fdb883ba7c7fec9183a368e66ff1cc44c3a2a494
Indunil1992 <indunil@adroitlogic.com>
2019-07-10 09:55:33 +0530

    k

--- indunilvirginia/lambda.js
+++ indunilvirginia/lambda.js
@@ -2,5 +2,5 @@ exports.handler = function(event, context, callback) {
 
     
     
-    callback(null, {"message": "Successfully executed"});
+    callback(null, {"message": "Successfully executed get "});
 }
