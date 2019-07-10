7aeadd77375e68d2937843bb06c41693136eddc6
Indunil1992 <indunil@adroitlogic.com>
2019-07-10 11:05:16 +0530

    sa

--- indunilvirginia/lambda.js
+++ indunilvirginia/lambda.js
@@ -3,35 +3,38 @@ let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
+
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/demoindu.fifo`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/gettest.fifo`,
         AttributeNames: ['All'],
-        MaxNumberOfMessages: '10',
-        VisibilityTimeout: '30',
-        WaitTimeSeconds: '0'
+        MaxNumberOfMessages: '100',
+        VisibilityTimeout: '300',
+        WaitTimeSeconds: '100',
+        MessageAttributeNames: ['sa', 'saa']
     }).promise()
         .then(receivedMsgData => {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
                 let receivedMessages = receivedMsgData.Messages;
                 receivedMessages.forEach(message => {
-                     console.log("Success with message ");
-                 console.log( data );
                     // your logic to access each message through out the loop. Each message is available under variable message 
                     // within this block
+                     console.log("message Success");
+                 console.log( message );
                 });
             } else {
-                 console.log("Success with no msg");
+                 console.log("Success");
                  //console.log( data );
                 // No messages to process
             }
         })
         .catch(err => {
-             console.log("not Success");
+             console.log("err happend");
                  console.log( err );
             // error handling goes here
         });
 
 
 
-    callback(null, { "message": "Successfully executed with nw fifo " });
+
+    callback(null, { "message": "Successfully executed with get" });
 }
